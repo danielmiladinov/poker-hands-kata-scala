1414b39a40953b697ad8c507097e2fb0a102839e
Daniel Miladinov <daniel.miladinov@gmail.com>
2017-06-06 21:21:44 -0400

    Finally implement the PokerHands game, passing the very first test given

--- src/main/scala/net/miladinov/poker/PokerHands.scala
+++ src/main/scala/net/miladinov/poker/PokerHands.scala
@@ -1,5 +1,14 @@
 package net.miladinov.poker
 
+import net.miladinov.poker.models.Hand
+import net.miladinov.poker.ranks.{Ranker, Tie, Winner}
+
 object PokerHands {
-  def playGame (input: String): String = null
+  def playGame (input: String): String = {
+    val Array(hand1, hand2) = input.split("  ").map(Hand.apply)
+    Ranker.rank(hand1, hand2) match {
+      case Winner(hand, title) => s"${hand.player} wins, with $title"
+      case Tie => Tie.toString
+    }
+  }
 }
--- src/test/scala/net/miladinov/poker/PokerHandsTest.scala
+++ src/test/scala/net/miladinov/poker/PokerHandsTest.scala
@@ -5,11 +5,11 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class PokerHandsTest extends FlatSpec with Matchers with TableDrivenPropertyChecks {
   val inputsAndExpectedOutputs = Table(
-    ("Input", "Expected Output"),
-    ( "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", "White wins, with High Card: Ace" ),
-    ( "Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S", "Black wins, with Full House: 4 over 2" ),
-    ( "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH", "Black wins, with High Card: 9" ),
-    ( "Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH", "Tie." )
+    ("Input",                                        "Expected Output"),
+    ("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", "White wins, with High Card: Ace"),
+    ("Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S", "Black wins, with Full House: Fours over Twos"),
+    ("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH", "Black wins, with High Card: Nine"),
+    ("Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH", "Tie")
   )
 
   forAll (inputsAndExpectedOutputs) { (input: String, expectedOutput: String) => {

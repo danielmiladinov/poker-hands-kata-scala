70089b82c4e9ae708dbc9dd668dd7ca407e4f62e
Daniel Miladinov <daniel.miladinov@gmail.com>
2017-06-09 00:40:15 -0400

    Deduplicate Hand.cardsByValueWithCounts and Hand.cardsBySuitWithCounts
    They are both just functions of by, just passing in different key
    selector functions.

--- src/main/scala/net/miladinov/poker/models/Hand.scala
+++ src/main/scala/net/miladinov/poker/models/Hand.scala
@@ -1,12 +1,12 @@
 package net.miladinov.poker.models
 
 case class Hand (player: String, cards: Set[Card]) {
-  def cardsByValueWithCounts: Map[(Value, Int), Set[Card]] = {
-    cards.groupBy(_.value).map({ case (v, cs) => ((v, cs.size), cs) })
-  }
+  def cardsByValueWithCounts: Map[(Value, Int), Set[Card]] = by(_.value)
 
-  def cardsBySuitWithCounts: Map[(Suit, Int), Set[Card]] = {
-    cards.groupBy(_.suit).map({ case (s, cs) => ((s, cs.size), cs) })
+  def cardsBySuitWithCounts: Map[(Suit, Int), Set[Card]] = by(_.suit)
+
+  private def by [K] (keyFn: Card => K): Map[(K, Int), Set[Card]] = {
+    cards.groupBy(keyFn).map({ case (k, cs) => ((k, cs.size), cs) })
   }
 }
 
